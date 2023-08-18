153cb62a2a4e3650d80ee5e914e080cbce69afa6
Edwin574 <owinoedwin574@gmail.com>
2023-08-18 10:34:15 +0300

    learning hooks

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './main-page.css';
-import Header from './Header';
+// import logo from "./logo.svg";
+import "./main-page.css";
+import Header from "./Header";
+import { useEffect, useState, useMemo } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FeatureHhouse from "./featured-house";
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
   return (
+    <Router>
     <div className="container">
-      <Header subtitle="Providing houses all over the world"/>
-    
+      <Header subtitle="Providing houses all over the world" />
     </div>
+    <Routes>
+    <Route path="/" element={<FeatureHhouse house={featuredHouse} />} />
+    </Routes>
+    </Router>
+    
   );
 }
 
